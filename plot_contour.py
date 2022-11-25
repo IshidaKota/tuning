8bb4dbab17caf60f65b7b52267eb24eb03e17b4d
r70240c <r70240c@ito-2.kyushu-u.sc>
2022-11-25 11:58:10 +0900

    fix salinity_1

--- plot_contour.py
+++ plot_contour.py
@@ -29,7 +29,7 @@ def find_closest_node(stn):
 
 #stn ={'chiba1buoy':(139.9542517,35.53703833),'chibaharo':(140.0233033,35.61095833),'kawasaki':(139.8340267,35.49019),'urayasu':(139.9417417,35.640085)}
 #stn_node = find_closest_node(stn)
-stn = {'chiba1buoy':136,'chibaharo':46,'kawasaki':222,'urayasu':51}
+stn = {'chiba1buoy':137,'chibaharo':47,'kawasaki':223,'urayasu':52}
 
 #fnames= [sys.argv[i] for i in range(1,len(sys.argv)-1)]##only before _0001.nc
 fnames = [sys.argv[1]]
@@ -41,16 +41,16 @@ for fname in fnames:
     #gauge = (140.05, 35.57)  # a sample (lon, lat) position for Estuary example
     #index = fvcom.closest_node(gauge).item()  ### ndarray -> scalar
     # print(index) # 295
-        fvcom = FileReader(ncfile, variables=['zeta', 'temp'], dims={'time': slice(0,8700), 'node': index})
-        time = Time(fvcom, figsize=(35,12), cmap = 'jet',cb_label='{} ({})'.format(fvcom.atts.temp.long_name,
-                                                                    fvcom.atts.temp.units))
+        fvcom = FileReader(ncfile, variables=['zeta', 'salinity'], dims={'time': slice(0,8700), 'node': index})
+        time = Time(fvcom, figsize=(35,12), cmap = 'jet',cb_label='{} ({})'.format(fvcom.atts.salinity.long_name,
+                                                                    fvcom.atts.salinity.units))
         z = unstructured_grid_depths(fvcom.grid.h, fvcom.data.zeta, fvcom.grid.siglay)
         ## print("z.shape", z.shape)
         ## fill_seabed makes the part of the plot below the seabed grey.
         ## We need to squeeze the data array since we've only extracted a single position.
         ## print(fvcom.grid.h.shape)
         ## print(fvcom.grid.h)
-        time.plot_surface(z, fvcom.data.temp, fill_seabed=True, h_offset=0, h_min=fvcom.grid.h,norm=Normalize(vmin=5, vmax=30))
+        time.plot_surface(z, fvcom.data.salinity, fill_seabed=True, h_offset=0, h_min=fvcom.grid.h,norm=Normalize(vmin=15, vmax=40))
         time.axes.set_ylabel('Depth (m)')
         time.figure.savefig('./png/contour/'+fname+'_'+place+'.png')
         print('savefig,path=./png/contour/'+fname+'_'+place+'.png')

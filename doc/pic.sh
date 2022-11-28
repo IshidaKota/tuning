3eb6464944206c59b1c3f0dd067f4681d49104d3
r70240c <r70240c@ito-1.kyushu-u.sc>
2022-11-28 23:45:47 +0900

    fix river_flux output

--- doc/pic.sh
+++ doc/pic.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-platex pic.tex
-dvipdfmx pic
+platex pic3.tex
+dvipdfmx pic3
+
+platex pic4.tex
+dvipdfmx pic4
+
+platex pic5.tex
+dvipdfmx pic5
+
+platex pic6.tex
+dvipdfmx pic6
+
+
